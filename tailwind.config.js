da63b1c2c38c977b8afa3f8d36f0d2c6238879f5
bita <nasrybita@gmail.com>
2024-09-07 15:58:25 +0430

    project slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,6 +32,18 @@ module.exports = {
         bg_dark1: '#242424',
         bg_dark2: '#000',
       },
+      keyframes: {
+        slide: {
+          '0%': { transform: 'translateX(0%)' },
+          '25%': { transform: 'translateX(-100%)' },
+          '50%': { transform: 'translateX(-200%)' },
+          '75%': { transform: 'translateX(-300%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
+      animation: {
+        slide: 'slide 15s ease infinite',
+      },
     },
   },
   plugins: [],
